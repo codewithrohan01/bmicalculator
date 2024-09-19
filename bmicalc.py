248e3e0b6280aa6ac545b280a6ca756672a710d7
codewithrohan01 <rs5799287@gmail.com>
2024-09-19 10:49:19 +0530

    Add files via upload

---
+++ bmicalc.py
@@ -0,0 +1,12 @@
+
+# bmi calculator
+
+height = float(input("enter your height in mtr :"))
+
+weight = float(input("enter your weight in Kgs :"))
+
+heisqr = height ** 2
+
+bmi = weight / heisqr
+
+print("your bmi :" + str(bmi))
